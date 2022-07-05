5a092514b3f05d65bd6e08b4e255f7f720f93511
olga.aheichyk <olga.aheichyk@gmail.com>
2022-07-05 19:08:22 +0300

    feat: add MainScreen and Card components to project

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
+import MainScreen from '../main-screen/main-screen';
+
+const TITLE = 'The Grand Budapest Hotel';
+const GENRE = 'Drama';
+const YEAR = 2012;
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return <MainScreen title={TITLE} genre={GENRE} year={YEAR} />;
 }
 
 export default App;
