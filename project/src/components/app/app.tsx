248cad0364a9f90c887a0bc96af791396f189116
olga.aheichyk <olga.aheichyk@gmail.com>
2022-07-06 13:31:00 +0300

    feat: add application routing using react-router-dom

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,31 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppRoute } from '../../const';
+import FilmScreen from '../film-screen/film-screen';
+import LoginScreen from '../login-screen/login-screen';
 import MainScreen from '../main-screen/main-screen';
+import MyListScreen from '../my-list-screen/my-list-screen';
+import NotFoundScreen from '../not-found-screen/not-found-screen';
+import PlayerScreen from '../player-screen/player-screen';
+import ReviewScreen from '../review-screen/review-screen';
 
 const TITLE = 'The Grand Budapest Hotel';
 const GENRE = 'Drama';
 const YEAR = 2012;
 
 function App(): JSX.Element {
-  return <MainScreen title={TITLE} genre={GENRE} year={YEAR} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Root} element={<MainScreen title={TITLE} genre={GENRE} year={YEAR} />} />
+        <Route path={AppRoute.Login} element={<LoginScreen />} />
+        <Route path={AppRoute.MyList} element={<MyListScreen />} />
+        <Route path={AppRoute.Film} element={<FilmScreen />} />
+        <Route path={AppRoute.Review} element={<ReviewScreen />} />
+        <Route path={AppRoute.Player} element={<PlayerScreen />} />
+        <Route path='*' element={<NotFoundScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
